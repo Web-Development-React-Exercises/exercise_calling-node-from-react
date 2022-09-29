c1e697c2eeb799d08bea86ad0ef5a363ab8a8002
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-29 10:28:19 +0300

    Finished exercise

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const cors = require('cors');
+app.use(cors());
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
